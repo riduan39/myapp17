580ed41d6b70880e364e53bce9cd3bf667206a07
riduan <mohammadriduan39@gmail.com>
2021-11-21 13:18:10 +0600

    riiii

---
+++ android/app/src/main/kotlin/com/example/myapp17/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myapp17
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:myapp17/pages/page%201.dart';
+import 'package:myapp17/pages/page%203.dart';
+import 'package:myapp17/pages/page%204.dart';
+import 'package:myapp17/pages/pages%202.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  var _currentIndex=0;
+  final pages=[
+  page1(), page2(), page4(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        index: _currentIndex,
+          color: Colors.lime,
+          buttonBackgroundColor: Colors.pinkAccent,
+          backgroundColor: Colors.white,
+          animationCurve: Curves.bounceOut,
+          animationDuration: Duration(microseconds: 700),
+          height: 60,
+          onTap: (index){
+          setState(() {
+            _currentIndex = index;
+          });
+          },
+          items: [Icon(Icons.people),
+            Icon(Icons.home),
+            Icon(Icons.add),
+      ]),
+      body: pages[
+      _currentIndex
+      ],
+    ));
+  }
+}
